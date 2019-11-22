5a62dc1a951b8f4dc1d37c886e22aac1bc6c36bf
Taras <50269675+TarasHryts@users.noreply.github.com>
2019-11-22 12:37:31 +0200

    Fix deposit (#27)
    * added exceptions
    
    * added fix travis
    
    * fixed deposit

--- src/main/java/com/team6/atm/atm/controller/AtmController.java
+++ src/main/java/com/team6/atm/atm/controller/AtmController.java
@@ -1,9 +1,11 @@
 package com.team6.atm.atm.controller;
 
 import com.team6.atm.atm.dto.AtmDto;
+import com.team6.atm.atm.dto.BanknotesDto;
 import com.team6.atm.atm.dto.DepositMoneyInAtmDto;
 import com.team6.atm.atm.dto.WithdrawMoneyDto;
 import com.team6.atm.atm.dto.util.AtmDtoUtil;
+import com.team6.atm.atm.dto.util.BanknotesDtoUtil;
 import com.team6.atm.atm.entity.Account;
 import com.team6.atm.atm.entity.Atm;
 import com.team6.atm.atm.entity.Banknotes;
@@ -14,8 +16,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.util.ArrayList;
 import java.util.List;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -61,11 +63,15 @@ public class AtmController {
                     message = "The resource you were trying to reach is not found")
     })
     @PostMapping("/deposit")
-    public void deposit(@Valid @RequestBody DepositMoneyInAtmDto depositMoneyInAtmDto) {
+    public void deposit(@RequestBody DepositMoneyInAtmDto depositMoneyInAtmDto) {
+        List<Banknotes> banknotesList = new ArrayList<>();
+        for (BanknotesDto banknote :depositMoneyInAtmDto.getBanknotesList()) {
+            banknotesList.add(BanknotesDtoUtil.createBanknotesFromDto(banknote));
+        }
         Account account = accountService.getById(depositMoneyInAtmDto.getAccountId()).orElseThrow(
                 () -> new AccountNotFoundException("Account not found."));
         Atm atm = atmService.getAtmById(depositMoneyInAtmDto.getAtmId()).orElseThrow();
-        atmService.deposit(atm, account, depositMoneyInAtmDto.getBanknotesList());
+        atmService.deposit(atm, account, banknotesList);
     }
 
     @ApiOperation(value = "withdraw", response = List.class)
@@ -79,7 +85,7 @@ public class AtmController {
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
     @PostMapping("/withdraw")
-    public void withdraw(@Valid @RequestBody WithdrawMoneyDto withdrawMoneyDto) {
+    public void withdraw(@RequestBody WithdrawMoneyDto withdrawMoneyDto) {
         Account account = accountService.getById(withdrawMoneyDto.getAccountId()).orElseThrow(
                 () -> new AccountNotFoundException("Account not found."));
         Atm atm = atmService.getAtmById(withdrawMoneyDto.getAtmId()).orElseThrow();
