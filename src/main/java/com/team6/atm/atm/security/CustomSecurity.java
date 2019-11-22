99bfec3ca951750b1303823b82f528771c75d3ad
AdminPC\Admin <admin2k7@gmail.com>
2019-11-22 11:29:35 +0200

    security

--- src/main/java/com/team6/atm/atm/security/CustomSecurity.java
+++ src/main/java/com/team6/atm/atm/security/CustomSecurity.java
@@ -1,13 +1,44 @@
 package com.team6.atm.atm.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class CustomSecurity extends WebSecurityConfigurerAdapter {
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}1")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/error**").hasRole("ADMIN")
+                .antMatchers("/account/transfer**").hasRole("USER")
+                .antMatchers("/account/**").hasRole("ADMIN")
+                .antMatchers("/atm/add**").hasRole("ADMIN")
+                .antMatchers("/atm/deposit**").hasRole("USER")
+                .antMatchers("/atm/withdraw**").hasRole("USER")
+                .antMatchers("/user/**").hasRole("ADMIN")
+                .antMatchers("/swagger**").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .and()
+                .csrf().disable();
     }
 }
